c2d16029a68f7dd2a9a81c4bdba80f0112e283dd
See <he15593557@gmail.com>
2024-02-27 11:31:20 +0800

    initialization

---
+++ README.md
@@ -0,0 +1,10 @@
+Mint Blockchain Docs
+Welcome to the Github for the Mint Dev Docs. If you'd like to contribute, simply open a PR with your changes and we'll review it as soon as possible.
+
+Install
+To install and start the docs locally, run the following command:
+
+```bash
+yarn install
+yarn dev
+```
---
+++ theme.config.jsx
@@ -0,0 +1,79 @@
+module.exports = {
+  logo:<svg width="126" height="32" viewBox="0 0 126 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M0 0C8.83656 0 16 7.16344 16 16C7.16344 16 0 8.83656 0 0Z" fill="#EBF5ED"/>
+  <path d="M32 32C23.1634 32 16 24.8366 16 16C24.8366 16 32 23.1634 32 32Z" fill="#EBF5ED"/>
+  <path d="M16 16C16 11.5817 19.5817 8 24 8C24 12.4183 20.4183 16 16 16Z" fill="#EBF5ED"/>
+  <path d="M16 16C16 20.4183 12.4183 24 8 24C8 19.5817 11.5817 16 16 16Z" fill="#EBF5ED"/>
+  <path d="M82.96 7C84.3407 7 85.46 5.88071 85.46 4.5C85.46 3.11929 84.3407 2 82.96 2C81.5793 2 80.46 3.11929 80.46 4.5C80.46 5.88071 81.5793 7 82.96 7Z" fill="#EBF5ED"/>
+  <path d="M72 6V28H75V30H65V28H68V8H67.7L59.5 25.58L51.3 8V28H54V30H46V28H48.7V6H46V4H54L60.5 17.94L67 4H75V6H72Z" fill="#EBF5ED"/>
+  <path d="M87.5 30V28H85V10L81 10.6L78.4 11V13L81 13.4V28H78.4V30H87.5Z" fill="#EBF5ED"/>
+  <path d="M111.7 30V28H109V16C109 12.68 106.32 10 103 10C100.04 10 97.58 12.14 97.1 14.94V10L93.1 10.6L90.5 11V13L93.1 13.4V28H90.5V30H99.6V28H97.1V16.96C97.1 14.76 98.84 13 101.04 13C103.24 13 105 14.76 105 16.96V28H102.6V30H111.7Z" fill="#EBF5ED"/>
+  <path d="M120.7 13V28H126V30H118.7C117.6 30 116.7 29.1 116.7 28V13H112.96V11H114.2C115.24 11 116.12 10.36 116.5 9.48L116.7 9.06L118.6 5H120.7V11H126V13H120.7Z" fill="#EBF5ED"/>
+  <mask id="mask0_1_91"  maskUnits="userSpaceOnUse" x="0" y="0" width="126" height="32">
+  <path d="M0 0C8.83656 0 16 7.16344 16 16C7.16344 16 0 8.83656 0 0Z" fill="#EBF5ED"/>
+  <path d="M32 32C23.1634 32 16 24.8366 16 16C24.8366 16 32 23.1634 32 32Z" fill="#EBF5ED"/>
+  <path d="M16 16C16 11.5817 19.5817 8 24 8C24 12.4183 20.4183 16 16 16Z" fill="#EBF5ED"/>
+  <path d="M16 16C16 20.4183 12.4183 24 8 24C8 19.5817 11.5817 16 16 16Z" fill="#EBF5ED"/>
+  <path d="M82.96 7C84.3407 7 85.46 5.88071 85.46 4.5C85.46 3.11929 84.3407 2 82.96 2C81.5793 2 80.46 3.11929 80.46 4.5C80.46 5.88071 81.5793 7 82.96 7Z" fill="#EBF5ED"/>
+  <path d="M72 6V28H75V30H65V28H68V8H67.7L59.5 25.58L51.3 8V28H54V30H46V28H48.7V6H46V4H54L60.5 17.94L67 4H75V6H72Z" fill="#EBF5ED"/>
+  <path d="M87.5 30V28H85V10L81 10.6L78.4 11V13L81 13.4V28H78.4V30H87.5Z" fill="#EBF5ED"/>
+  <path d="M111.7 30V28H109V16C109 12.68 106.32 10 103 10C100.04 10 97.58 12.14 97.1 14.94V10L93.1 10.6L90.5 11V13L93.1 13.4V28H90.5V30H99.6V28H97.1V16.96C97.1 14.76 98.84 13 101.04 13C103.24 13 105 14.76 105 16.96V28H102.6V30H111.7Z" fill="#EBF5ED"/>
+  <path d="M120.7 13V28H126V30H118.7C117.6 30 116.7 29.1 116.7 28V13H112.96V11H114.2C115.24 11 116.12 10.36 116.5 9.48L116.7 9.06L118.6 5H120.7V11H126V13H120.7Z" fill="#EBF5ED"/>
+  </mask>
+  <g mask="url(#mask0_1_91)">
+  <rect width="126" height="32" fill="#30BF54"/>
+  </g>
+  </svg>
+  ,
+  project: {
+    link: "https://github.com/Mint-Blockchain",
+  },
+  docsRepositoryBase: "https://github.com/Mint-Blockchain/document/tree/master",
+  useNextSeoProps(){
+    return { titleTemplate: '%s – Mint Blockchain' }
+  },
+  chat: {
+    link: 'https://discord.gg/mint-blockchain',
+    icon: (
+      <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 127.14 96.36" ><g id="图层_2" data-name="图层 2"><g id="Discord_Logos" data-name="Discord Logos"><g id="Discord_Logo_-_Large_-_White" data-name="Discord Logo - Large - White"><path d="M107.7,8.07A105.15,105.15,0,0,0,81.47,0a72.06,72.06,0,0,0-3.36,6.83A97.68,97.68,0,0,0,49,6.83,72.37,72.37,0,0,0,45.64,0,105.89,105.89,0,0,0,19.39,8.09C2.79,32.65-1.71,56.6.54,80.21h0A105.73,105.73,0,0,0,32.71,96.36,77.7,77.7,0,0,0,39.6,85.25a68.42,68.42,0,0,1-10.85-5.18c.91-.66,1.8-1.34,2.66-2a75.57,75.57,0,0,0,64.32,0c.87.71,1.76,1.39,2.66,2a68.68,68.68,0,0,1-10.87,5.19,77,77,0,0,0,6.89,11.1A105.25,105.25,0,0,0,126.6,80.22h0C129.24,52.84,122.09,29.11,107.7,8.07ZM42.45,65.69C36.18,65.69,31,60,31,53s5-12.74,11.43-12.74S54,46,53.89,53,48.84,65.69,42.45,65.69Zm42.24,0C78.41,65.69,73.25,60,73.25,53s5-12.74,11.44-12.74S96.23,46,96.12,53,91.08,65.69,84.69,65.69Z"/></g></g></g></svg>)
+  },
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta
+        key="og:title"
+        property="og:title"
+        content="Mint Blockchain Documentation"
+      />
+      <meta
+        name="description"
+        property="og:description"
+        content={`Mint is the L2 blockchain for NFTs.`}
+      />
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta
+        key="og:image"
+        property="og:image"
+        content={`https://docs.mintchain.io/assets/document-brand.png`}
+      />
+      <meta property="og:url" content={"https://docs.mintchain.io"} />
+      <meta content="Mint Docs" property="og:site_name" />
+      <meta content="Mint Docs" name="apple-mobile-web-app-title" />
+      <meta content="Mint Docs" name="application-name" />
+      <meta content="#30bf54" name="msapplication-TileColor" />
+      <meta name="keywords" content={`NFT | Layer2 | Documentation`} />
+      <link rel="shortcut icon" href="/favicon.ico" />
+    </>
+  ),
+  footer: {
+    text: (
+      <span>
+        MIT {new Date().getFullYear()} ©{' '}
+        <a href="https://mintchain.io" target="_blank">
+          Mint
+        </a>
+        .
+      </span>
+    )
+  }
+};
